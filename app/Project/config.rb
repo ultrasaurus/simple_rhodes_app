0e4db6abbe065062c3fa08e252ceb577cf28c1f0
Sarah Allen <sarah@ultrasaurus.com>
2009-07-14 19:12:54 -0700

    added link aand gratuitous UI changes, new source name

--- app/Project/config.rb
+++ app/Project/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("Project", {"url"=>"http://localhost:3000/apps/BlazingCloud/sources/BlazingCloudREST", "source_id"=>1285})
+Rho::RhoConfig::add_source("Project", {"url"=>"http://localhost:3000/apps/BlazingCloudForTester/sources/BlazingCloudTester", "source_id"=>1285})
